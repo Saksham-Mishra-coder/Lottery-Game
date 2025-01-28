f6240962e3e09994b6e4fcf15891e5cb1dba41c6
Saksham Mishra <sakshammishra2023@gmail.com>
2025-01-28 09:45:44 +0530

    Create Lottery Game :)

---
+++ lottery-game/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+import Lottery from './Lottery'
+import { sum } from './helper'
+
+function App() {
+ 
+  function winCondition(ticket){
+    return sum(ticket)===15;
+  }
+  return (
+  
+    <>
+      <Lottery n ={3} winCondition={winCondition}/>
+    </>
+  )
+}
+
+export default App
---
+++ lottery-game/src/Lottery.jsx
@@ -0,0 +1,21 @@
+import { genRandomNum,sum } from "./helper.js";
+import { useState } from "react";
+import Ticket from "./Ticket";
+
+export default function Lottery({n=3, winCondition}){
+    let [ticket,setTicket]= useState(genRandomNum(n));
+    let isWinning = winCondition(ticket);
+    let buyTicket= ()=>{
+        setTicket(genRandomNum(n));
+    }
+    return(
+        <div>
+            <h1>Lottery Game!</h1>
+            <br />
+            <p>If your ticket number's sum is 15 then you won :)</p>
+            <Ticket ticket={ticket}/>
+            <button onClick={buyTicket}>Buy New Ticket</button>
+            <h3>{isWinning && "Congratulation,you won :)"}</h3>
+        </div>
+    )
+}
